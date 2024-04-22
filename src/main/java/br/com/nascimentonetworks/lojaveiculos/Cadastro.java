fb1cdc7b9985e376f635388d569a6d2626260c3d
Carlos Nascimento <carlosnascimento.ms@outlook.com>
2024-04-22 07:25:40 -0300

    Initial commit

---
+++ src/main/java/br/com/nascimentonetworks/lojaveiculos/Cadastro.java
@@ -0,0 +1,62 @@
+package br.com.nascimentonetworks.lojaveiculos;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import java.util.Scanner;
+
+public class Cadastro {
+	
+	public static void main(String[] args) {
+	    String url = "jdbc:postgresql://localhost:5432/postgres";
+	    String username = "postgres";
+	    String password = "123";
+
+	    try (Connection conn = DriverManager.getConnection(url, username, password)) {
+	        Scanner sc = new Scanner(System.in);
+
+	        // Solicitar dados do usuário
+	        System.out.println("Informe o código do veículo: ");
+	        int codigo = sc.nextInt();
+	        sc.nextLine(); // Consumir nova linha pendente
+	        
+	        System.out.println("Informe a marca do carro: ");
+	        String marca = sc.nextLine();
+	        
+	        System.out.println("Informe o modelo do carro: ");
+	        String modelo = sc.nextLine();
+	        
+	        System.out.println("Informe o ano de fabricação do veículo: ");
+	        int ano = sc.nextInt();
+	        
+	        System.out.println("Informe o preço do veículo: ");
+	        int preco = sc.nextInt();
+	        sc.nextLine(); // Consumir nova linha pendente
+	        
+	        System.out.println("Informe a placa do veículo: ");
+	        String placa = sc.nextLine();
+
+	        // Preparar e executar a query SQL para inserir dados
+	        String sql = "INSERT INTO carros(codigo, marca, modelo, ano, preco, placa) VALUES (?, ?, ?, ?, ?, ?)";
+	        try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+	            preparedStatement.setInt(1, codigo);
+	            preparedStatement.setString(2, marca);
+	            preparedStatement.setString(3, modelo);
+	            preparedStatement.setInt(4, ano);
+	            preparedStatement.setInt(5, preco);
+	            preparedStatement.setString(6, placa);
+
+	            // Executar a query
+	            int rowsAffected = preparedStatement.executeUpdate();
+	            System.out.println(rowsAffected + " linha inseridas no banco de dados com sucesso");
+	        } catch (SQLException e) {
+	            System.err.println("Erro ao gravar no banco de dados: " + e.getMessage());
+	        }
+	    } catch (SQLException e) {
+	        System.err.println("Erro ao estabelecer conexão com Banco de Dados: " + e.getMessage());
+	    }
+	}
+}
+
---
+++ src/main/java/br/com/nascimentonetworks/lojaveiculos/Main.java
@@ -0,0 +1,11 @@
+package br.com.nascimentonetworks.lojaveiculos;
+
+/**
+ *
+ * @author Your name
+ */
+public class Main {
+    public static void main(String... param) {
+        System.out.println("Starting application...");
+    }
+}
